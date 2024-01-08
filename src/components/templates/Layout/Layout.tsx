ae99f2f20b7a2f3cda254ef308e04ccb59aff42f
satryaputra <muhammadsatrya12@gmail.com>
2024-01-09 01:17:46 +0700

    update(style): min-h-screen -> h-screen

--- src/components/templates/Layout/Layout.tsx
+++ src/components/templates/Layout/Layout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <main className="min-h-screen bg-gray-50 overflow-y-auto">
-      <div className="max-w-[480px] mx-auto bg-white">
+    <main className="h-screen bg-gray-50 overflow-y-auto">
+      <div className="max-w-[480px] h-full mx-auto bg-white">
         <Outlet />
       </div>
     </main>
