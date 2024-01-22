4238c481e70bce22f4cdf6c5220788b1bf70b062
fnzadhitya <137802138+fnzadhitya@users.noreply.github.com>
2024-01-22 08:30:46 +0700

    update

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,10 +1,8 @@
-
-import { useNavigate } from "react-router-dom";
-import { IconPhoneCall, IconBuildingSkyscraper, IconUser, IconUserBolt, IconEdit } from "@tabler/icons-react";
-import imgPP from "../../assets/pp.jpg";
+import { Link, useNavigate } from 'react-router-dom';
+import {  IconCircleArrowLeftFilled, IconEdit } from '@tabler/icons-react';
+import imgPP from '../../assets/pp.jpg';
 // import imgPP from '../../assets/homePage.png'
-import imgBG from "../../assets/background.png";
-import { Button } from "../../components/molecules";
+import imgBG from '../../assets/background.png';
 
 export default function ProfilePage() {
   const navigate = useNavigate();
@@ -14,32 +12,30 @@ export default function ProfilePage() {
   // };
 
   const handleEditProfile = () => {
-    navigate("/update-profile");
+    navigate('/update-profile');
   };
 
   const Nama = "Eka Budiono Putra Siregar";
   const Email = "ekasiregar@gmail.com";
   const Company = "UT School";
-  const NickName = "Putra";
-  const EmergencyName = "Bambang Siregar";
-  const EmergencyContact = "+62 893523414";
+  const Contact = "+62 893523414";
 
   return (
     <>
-      <div className="">
-        <div className="top-0 px-4 absolute flex items-center">
-          {/* <Button onClick={handleGoBack}>
+      <div className="flex flex-col w-full" >
+        <div className="flex items-center justify-between top-0 px-4 relative z-10">
+          <Link to="/dashboard">
             <IconCircleArrowLeftFilled className="text-black text-xl" />
-          </Button> */}
+          </Link>
           <div className="flex-grow text-center">
-            <h1 className="text-center text-2xl text-white  font-semibold pt-10 mb-10">Profile</h1>
+            <h1 className="text-center text-2xl text-white font-semibold pt-10 mb-10">Profile</h1>
           </div>
-          <Button onClick={handleEditProfile}>
+          <Link to="/update-profile">
             <IconEdit className="text-white text-xl" />
-          </Button>
+          </Link>
         </div>
 
-        <img src={imgBG} alt="" className="" />
+        <img src={imgBG} alt="" className="absolute top-0 w-full -z-0" />
 
         <div className="top-10 ml-6 flex items-center justify-center relative z-50">
           <div className="order-1">
@@ -53,45 +49,45 @@ export default function ProfilePage() {
 
         <div className="mt-32 flex items-center justify-center">
           <div className="bg-white-100 p-8 w-full">
+
             <div className="flex items-center mb-6">
-              <div className="bg-white rounded-full p-3 border border-gray-300 shadow-md mr-4">
-                <IconBuildingSkyscraper className="text-green-500 text-xl" size={32} />
-              </div>
               <div className="flex flex-col w-full">
-                <div className="text-lg font-normal mb-0">Company/Branch</div>
-                <div className="px-3 py-1 text-lg font-semibold mb-2 w-full border border-gray-300 rounded-md">{Company}</div>
+                <div className="text-lg font-normal mb-0">Instansi Perusahaan</div>
+                <div className="px-3 py-1 text-lg font-semibold mb-2 w-full border border-gray-300 rounded-md d-flex justify-content-between">
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    {Company}
+                    <IconEdit onClick={handleEditProfile} style={{ color: 'blue', marginLeft: 'auto', cursor: 'pointer' }} />
+                  </div>
+                </div>
               </div>
             </div>
 
             <div className="flex items-center mb-6">
-              <div className="bg-white rounded-full p-3 border border-gray-300 shadow-md mr-4">
-                <IconUser className="text-green-500 text-xl" size={32} />
-              </div>
               <div className="flex flex-col w-full">
-                <div className="text-lg font-normal mb-0">Nick Name</div>
-                <div className="px-3 py-1 text-lg font-semibold mb-2 w-full border border-gray-300 rounded-md">{NickName}</div>
+                <div className="text-lg font-normal mb-0">Nama</div>
+                <div className="px-3 py-1 text-lg font-semibold mb-2 w-full border border-gray-300 rounded-md">{Nama}</div>
               </div>
             </div>
 
             <div className="flex items-center mb-6">
-              <div className="bg-white rounded-full p-3 border border-gray-300 shadow-md mr-4">
-                <IconUserBolt className="text-green-500 text-xl" size={32} />
-              </div>
               <div className="flex flex-col w-full">
-                <div className="text-lg font-normal mb-0">Emergency Name</div>
-                <div className="px-3 py-1 text-lg font-semibold mb-2 w-full border border-gray-300 rounded-md">{EmergencyName}</div>
+                <div className="text-lg font-normal mb-0">Alamat Email</div>
+                <div className="px-3 py-1 text-lg font-semibold mb-2 w-full border border-gray-300 rounded-md">{Email}</div>
               </div>
             </div>
 
             <div className="flex items-center mb-6">
-              <div className="bg-white rounded-full p-3 border border-gray-300 shadow-md mr-4">
-                <IconPhoneCall className="text-green-500 text-xl" size={32} />
-              </div>
               <div className="flex flex-col w-full">
-                <div className="text-lg font-normal mb-0">Emergency Contact</div>
-                <div className="px-3 py-1 text-lg font-semibold mb-2 w-full border border-gray-300 rounded-md">{EmergencyContact}</div>
+                <div className="text-lg font-normal mb-0">Kontak</div>
+                <div className="px-3 py-1 text-lg font-semibold mb-2 w-full border border-gray-300 rounded-md d-flex justify-content-between">
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    {Contact}
+                    <IconEdit onClick={handleEditProfile} style={{ color: 'blue', marginLeft: 'auto', cursor: 'pointer' }} />
+                  </div>
+                </div>
               </div>
             </div>
+
           </div>
         </div>
       </div>
