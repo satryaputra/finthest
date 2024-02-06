ba8145663f2714744058255ec690c7c88af487de
satputraya <satputraya@gmail.com>
2024-02-06 20:38:17 +0700

    feat: password type eye

--- src/components/molecules/FormField/FormField.tsx
+++ src/components/molecules/FormField/FormField.tsx
@@ -8,6 +8,8 @@ import {
   FormField as _FormField,
 } from "@/components/ui/form";
 import { cn } from "@/lib/utils";
+import { Eye, EyeOff } from "lucide-react";
+import React from "react";
 
 interface FormFieldProps extends InputProps {
   name: string;
@@ -20,6 +22,8 @@ export default function FormField(props: FormFieldProps) {
     control,
     formState: { errors },
   } = useFormContext();
+
+  const [isEyeOpen, setIsEyeOpen] = React.useState<boolean>(false);
   return (
     <_FormField
       control={control}
@@ -28,16 +32,35 @@ export default function FormField(props: FormFieldProps) {
         <FormItem>
           <FormLabel className="!font-normal !text-xs">{label}</FormLabel>
           <FormControl>
-            <Input
-              {...field}
-              placeholder={placeholder}
-              className={cn(
-                "!ring-offset-0 !ring-0 focus:!ring-0 border-2 !border-neutral-300 focus:!border-neutral-500",
-                className,
-                Object.keys(errors).includes(name) && "!border-red-300"
-              )}
-              {...otherInputProps}
-            />
+            <div className="relative">
+              <Input
+                {...field}
+                {...otherInputProps}
+                placeholder={placeholder}
+                className={cn(
+                  "!ring-offset-0 !ring-0 focus:!ring-0 border-2 !border-neutral-300 focus:!border-neutral-500",
+                  className,
+                  Object.keys(errors).includes(name) && "!border-red-300"
+                )}
+                type={isEyeOpen ? "text" : otherInputProps.type}
+              />
+              {otherInputProps.type === "password" &&
+                (isEyeOpen ? (
+                  <Eye
+                    className="absolute top-1/2 -translate-y-1/2 right-4 h-5 w-5 text-neutral-700 cursor-pointer"
+                    onClick={() => {
+                      setIsEyeOpen(false);
+                    }}
+                  />
+                ) : (
+                  <EyeOff
+                    className="absolute top-1/2 -translate-y-1/2 right-4 h-5 w-5 text-neutral-700 cursor-pointer"
+                    onClick={() => {
+                      setIsEyeOpen(true);
+                    }}
+                  />
+                ))}
+            </div>
           </FormControl>
           <FormMessage className="text-xs font-normal !mt-[.3rem] pl-1" />
         </FormItem>
