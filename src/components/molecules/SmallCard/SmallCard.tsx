e62a92bb3a6e234716d1cf8c9f384efcc036123f
faturrahman82 <newaqount99@gmail.com>
2024-02-07 20:17:25 +0700

    add info card calculation

--- src/components/molecules/SmallCard/SmallCard.tsx
+++ src/components/molecules/SmallCard/SmallCard.tsx
@@ -22,15 +22,17 @@ export default function SmallCard() {
               <Card className="w-full h-[7rem] rounded-3xl [background:linear-gradient(180deg,rgb(66.83,218.88,41.95)_0%,rgb(80.84,172.13,65.98)_100%)]">
                 <CardContent className="w-full h-full flex flex-col bg-[url('/src/assets/ImageCard.svg')]">
                   <div className="flex h-full flex-col mt-3 gap-3">
-                    <p className="font-bold text-white text-2xl">Beli Motor</p>
+                    <p className="font-bold text-white text-xl ">Beli Motor</p>
                     <Progress
                       value={33}
                       className="h-[.60rem]"
                       indicatorClassName="bg-blue-400"
                     />
                     <div className="flex justify-between text-white text-sm">
-                      <p className="font-light ">Target</p>
-                      <span className="font-medium">Rp. 900rb/Rp. 10jt</span>
+                      <p className="font-light sm:text-sm">Target</p>
+                      <span className="font-medium sm:text-sm">
+                        Rp. 900rb/Rp. 10jt
+                      </span>
                     </div>
                   </div>
                 </CardContent>
--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -8,7 +8,7 @@ import {
   SmallCard,
 } from "@/components/molecules";
 import ProfilePicture from "../../assets/ProfilePicture.svg";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   Dialog,
   DialogContent,
@@ -24,11 +24,22 @@ export default function DashboardPage() {
 
   const methods = useForm();
 
+  const onSubmitHandler = (data: any) => {
+    console.log(data);
+  };
+  const onSubmitEdit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <div className="w-full flex flex-col px-7">
       <div className="w-full flex items-center space-x-3 mt-6">
-        <img src={ProfilePicture} alt="" className="w-14 h-14" />
-        <p className="font-semibold text-lg">Eka Budiono Putra Siregar</p>
+        <Link to={"/profile"}>
+          <img src={ProfilePicture} alt="" className="sm:w-14 w-10" />
+        </Link>
+        <p className="font-semibold text-sm sm:text-lg">
+          Eka Budiono Putra Siregar
+        </p>
       </div>
       <div className="w-full flex items-center justify-center mt-5">
         <div className="w-full h-[29.5rem] flex flex-col bg-white rounded-3xl shadow-[0px_0px_1px_#0000000a,0px_2px_6px_#0000000a,0px_16px_24px_#0000000f]">
@@ -36,10 +47,13 @@ export default function DashboardPage() {
             <div className="flex space-x-2 items-center">
               <p className="font-medium text-lg">Wish List</p>
               <Dialog>
-                <DialogTrigger asChild>
-                  <button className="bg-[#FFC145] rounded-full p-1 cursor-pointer">
+                <DialogTrigger
+                  asChild
+                  className="bg-[#FFC145] rounded-full p-1 cursor-pointer"
+                >
+                  <div>
                     <Plus size={12} />
-                  </button>
+                  </div>
                 </DialogTrigger>
                 <DialogContent className="w-96 h-[26rem]">
                   <DialogHeader className="">
@@ -49,7 +63,7 @@ export default function DashboardPage() {
                   </DialogHeader>
                   <div className="items-center">
                     <FormProvider {...methods}>
-                      <form>
+                      <form onSubmit={methods.handleSubmit(onSubmitHandler)}>
                         <div className="w-full">
                           <FormField
                             name="judul"
@@ -64,14 +78,16 @@ export default function DashboardPage() {
                           placeholder="Target"
                           className="text-sm "
                         />
+                        <DialogFooter className="mt-10">
+                          <div className="w-full grid ">
+                            <Button type="submit" className="py-6 text-xl">
+                              Simpan
+                            </Button>
+                          </div>
+                        </DialogFooter>
                       </form>
                     </FormProvider>
                   </div>
-                  <DialogFooter>
-                    <div className="w-full grid ">
-                      <Button className="py-6 text-xl">Simpan</Button>
-                    </div>
-                  </DialogFooter>
                 </DialogContent>
               </Dialog>
             </div>
@@ -85,7 +101,7 @@ export default function DashboardPage() {
                 <p className="font-normal text-sm text-[#AEAEAE]">
                   Gaji Per bulan
                 </p>
-                <span className="font-bold text-2xl text-[#408FFF]">
+                <span className="font-bold text-xl sm:text-2xl text-[#408FFF]">
                   Rp. 5.000.000
                 </span>
               </div>
@@ -94,12 +110,43 @@ export default function DashboardPage() {
                   Jumlah tabungan
                 </p>
                 <div className="flex items-center space-x-2">
-                  <span className="font-semibold text-xl text-[#439A86]">
+                  <span className="font-semibold text-lg sm:text-xl text-[#439A86]">
                     Rp. 3.000.000
                   </span>
-                  <div className="bg-[#FFC145] rounded-full p-1 cursor-pointer">
-                    <Plus size={12} />
-                  </div>
+                  <Dialog>
+                    <DialogTrigger className="bg-[#FFC145] rounded-full p-1 cursor-pointer">
+                      <Plus size={12} />
+                    </DialogTrigger>
+                    <DialogContent className="w-96 h-[26rem]">
+                      <DialogDescription className="text-lg">
+                        Edit jumlah tabungan dari sebelumnya jika ada melakukan
+                        penabungan uang hari ini.
+                      </DialogDescription>
+                      <div className="items-center">
+                        <FormProvider {...methods}>
+                          <form
+                            onSubmit={methods.handleSubmit(onSubmitEdit)}
+                          >
+                            <div className="w-full">
+                              <FormField
+                                name="uang"
+                                label="Edit Jumlah Tabungan"
+                                placeholder="Jumblah"
+                                className="text-sm "
+                              />
+                            </div>
+                            <DialogFooter className="mt-10">
+                              <div className="w-full grid ">
+                                <Button type="submit" className="py-6 text-xl">
+                                  Simpan
+                                </Button>
+                              </div>
+                            </DialogFooter>
+                          </form>
+                        </FormProvider>
+                      </div>
+                    </DialogContent>
+                  </Dialog>
                 </div>
               </div>
               <div className="flex flex-col ">
@@ -107,14 +154,16 @@ export default function DashboardPage() {
                   Jumlah pinjaman
                 </p>
                 <div className="flex items-center justify-between">
-                  <span className="font-semibold items-center text-xl text-[#BB4430]">
+                  <span className="font-semibold items-center text-md sm:text-xl text-[#BB4430]">
                     Rp. 2.220.000
                   </span>
                   <div className="flex flex-col gap-2">
-                    <p className="font-medium text-xs">Rp. 900rb / Rp. 2.2jt</p>
+                    <p className="font-medium text-[10px] sm:text-xs">
+                      Rp. 900rb / Rp. 2.2jt
+                    </p>
                     <Progress
                       value={40}
-                      className="w-[10rem] h-[.60rem]"
+                      className="sm:w-[10rem] sm:h-[.60rem]"
                       indicatorClassName="[background:linear-gradient(180deg,rgb(32,157,67)_0%,rgb(42.94,194.44,85.36)_100%)]"
                     />
                   </div>
