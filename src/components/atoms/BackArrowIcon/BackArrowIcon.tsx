cf361de58cace0da2c046c215f2d131078f64e6b
satputraya <satputraya@gmail.com>
2024-02-08 10:27:11 +0700

    update(style): BackArrowIcon

--- src/components/atoms/BackArrowIcon/BackArrowIcon.tsx
+++ src/components/atoms/BackArrowIcon/BackArrowIcon.tsx
@@ -1,11 +1,26 @@
+import { cn } from "@/lib/utils";
 import { ChevronLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
-export default function BackArrowIcon() {
+export default function BackArrowIcon({
+  size = 18,
+  classname,
+}: {
+  size?: string | number | undefined;
+  classname?: string;
+}) {
+  const navigate = useNavigate();
   return (
-    <div className="flex bg-[#475569] rounded-full text-white cursor-pointer">
-      <div className="flex justify-center items-center p-1">
-        <ChevronLeft size={25} />
-      </div>
+    <div
+      className={cn(
+        "flex items-center justify-center p-1 bg-[#475569] rounded-full text-white cursor-pointer hover:ring-[5px] hover:ring-[#475569]/20 transition",
+        classname
+      )}
+      onClick={() => {
+        navigate(-1);
+      }}
+    >
+      <ChevronLeft size={size} />
     </div>
   );
 }
